9aeee7444ae7d0485a4db177882bbff257ff115c
Roberto Livi <robertolivi@Robertos-MBP.home>
2020-04-22 18:35:43 -0400

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "secret"
   end
 
   get "/" do
