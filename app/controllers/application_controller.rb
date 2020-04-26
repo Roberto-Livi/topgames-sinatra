6b7ec81e63a75cd2d73c3dffb0df3427dbb0de24
Roberto Livi <robertolivi@Robertos-MBP.home>
2020-04-26 14:04:32 -0400

    user able to create a new list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -93,14 +93,38 @@ class ApplicationController < Sinatra::Base
     else
       game = Game.create(:first_game => params[:first_game], :second_game => params[:second_game], :third_game => params[:third_game], :fourth_game => params[:fourth_game], :fifth_game => params[:fifth_game], :user_id => params[:user_id], :list_name => params[:list_name])
 
-      redirect "/users_games_selection/#{game.id}"
+      redirect "/users_games_selection/#{user.id}"
     end
+  end
+
+  get '/new_games_list' do
+    if Helpers.is_logged_in?(session)
+      erb :"/games/new_game_list"
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/new_games_list' do
+    params.each do |key, value|
+      if value.empty?
+        redirect "/new_games_list"
+      end
+    end
+
+    @user = Helpers.current_user(session)
+    params[:user_id] = @user.id
+
+    new_list = Game.create(:first_game => params[:first_game], :second_game => params[:second_game], :third_game => params[:third_game], :fourth_game => params[:fourth_game], :fifth_game => params[:fifth_game], :user_id => params[:user_id], :list_name => params[:list_name])
+    @user.games << new_list
+
+    redirect "/users_games_selection/#{@user.id}"
+
   end
 
   get '/users_games_selection/:id' do
     @user = Helpers.current_user(session)
     params[:user_id] = @user.id
-    @games = Game.find(params[:id])
     if Helpers.is_logged_in?(session)
       erb :"/games/users_games_selection"
     else
